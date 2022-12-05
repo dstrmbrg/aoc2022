8c65188dc516b4a0450b9349ea2c20acf7c71765
Daniel Strömberg <daniel.stromberg1@gmail.com>
2022-12-05 20:33:51 +0100

    Day 5

---
+++ lib/puzzles/day05.ex
@@ -0,0 +1,52 @@
+defmodule Day05 do
+
+  def part1(), do: solve(true)
+  def part2(), do: solve(false)
+
+  defp solve(reverse_move_order) do
+    InputReader.read(5)
+    |> parse()
+    |> then(fn %{state: state, instructions: instructions} -> Enum.reduce(instructions, state, &(apply_instruction(&2, &1, reverse_move_order))) end)
+    |> Enum.reduce("", &(&2 <> hd(&1)))
+    |> IO.puts()
+  end
+
+  defp apply_instruction(state, %{amount: amount, from_index: from_index, to_index: to_index}, reverse_move_order) do
+    state
+    |> take_crates(amount, from_index, reverse_move_order)
+    |> then(fn move ->
+      List.update_at(state, to_index, &(move ++ &1))
+      |> List.update_at(from_index, &(&1 -- move))
+    end)
+  end
+
+  defp take_crates(state, amount, from_index, true), do: Enum.take(Enum.at(state, from_index), amount) |> Enum.reverse()
+  defp take_crates(state, amount, from_index, false), do: Enum.take(Enum.at(state, from_index), amount)
+
+  defp parse(input) do
+    input
+    |> String.split("\r\n\r\n")
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [first, second] -> %{state: parse_initial_state(first), instructions: parse_instructions(second)} end)
+    |> hd()
+  end
+
+  defp parse_initial_state(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.take_while(fn x -> !String.starts_with?(x, " 1") end)
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&(Enum.chunk_every(&1, 4) |> Enum.map(fn x -> to_string(x) |> String.replace([" ", "[", "]"], "") end)))
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&(Enum.filter(&1, fn x -> x != "" end)))
+  end
+
+  defp parse_instructions(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(&(Regex.scan(~r/\d+/, &1) |> Enum.map(fn x -> hd(x) end)))
+    |> Enum.map(&(Enum.map(&1, fn x -> String.to_integer(x) end)))
+    |> Enum.map(&(%{amount: Enum.at(&1, 0), from_index: Enum.at(&1, 1) - 1, to_index: Enum.at(&1, 2) - 1}))
+  end
+end
